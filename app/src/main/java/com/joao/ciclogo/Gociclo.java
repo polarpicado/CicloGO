8aa8dec2fc4eac715a0178f23d55d477d131b976
MayomiMedina <medinahuallpamayomi@gmail.com>
2022-07-06 19:04:06 -0500

    logo y animación de inicio- Mayomi

---
+++ app/src/main/java/com/joao/ciclogo/Gociclo.java
@@ -0,0 +1,30 @@
+package com.joao.ciclogo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Gociclo extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gociclo);
+
+        TimerTask tarea= new TimerTask(){
+            @Override
+            public void run(){
+            Intent intent= new Intent(Gociclo.this,MainActivity.class);
+            startActivity(intent);
+            finish();
+            }
+        };
+        Timer tiempo= new Timer();
+        tiempo.schedule(tarea,5000);
+    }
+}
