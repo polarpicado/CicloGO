250e089efc9c3cb478f6c5406f013924275c4438
Gummimon <darioasqv@gmail.com>
2022-07-07 00:08:48 -0500

    Cambios Dario

--- app/src/main/java/com/joao/ciclogo/MainActivity.java
+++ app/src/main/java/com/joao/ciclogo/MainActivity.java
@@ -1,16 +1,29 @@
 package com.joao.ciclogo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_login, btn_register;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        asignarReferencias();
+        btn_login.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this,SegrutaActivity.class);
+            startActivity(intent);
+        });
+    }
+    private void asignarReferencias(){
+        btn_login = findViewById(R.id.btn_login);
+        btn_register = findViewById(R.id.btn_register);
     }
 }
---
+++ app/src/main/java/com/joao/ciclogo/SegrutaActivity.java
@@ -0,0 +1,16 @@
+package com.joao.ciclogo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SegrutaActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segruta);
+    }
+}
