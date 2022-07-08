ab29ecdf7b3287f692baaf051c6b112cf8d337ff
MayomiMedina <medinahuallpamayomi@gmail.com>
2022-07-07 22:15:54 -0500

    menu y direccionamiento

--- app/src/main/java/com/joao/ciclogo/menuActivity.java
+++ app/src/main/java/com/joao/ciclogo/menuActivity.java
@@ -1,6 +1,8 @@
 package com.joao.ciclogo;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
@@ -8,16 +10,19 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
+import android.opengl.EGLExt;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 
 
 
 import com.google.android.material.navigation.NavigationView;
 
-public class menuActivity extends AppCompatActivity {
+public class menuActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawerLayout;
-    private NavigationView maNavigationView;
+    private NavigationView navigationView;
+    private  Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,39 +30,58 @@ public class menuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_menu);
         setToolBar();
         drawerLayout = findViewById(R.id.drawer_layout);
-        maNavigationView = findViewById(R.id.navview);
-    }
+        navigationView = findViewById(R.id.navview);
+        toolbar=findViewById(R.id.toolbar);
+        navigationView.setNavigationItemSelectedListener(this);
+        setSupportActionBar(toolbar);
 
+
+
+    }
     private void setToolBar() {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_home);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
+        super.onBackPressed();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+switch (menuItem.getItemId()){
+    case R.id.menu_nueva_rutas:
+        Intent intent= new Intent(menuActivity.this,SegrutaActivity.class);
+        startActivity(intent);
+        break;
+}
+        switch (menuItem.getItemId()){
+            case R.id.menu_comentar_ruta:
+                Intent intent= new Intent(menuActivity.this,SegrutaActivity.class);
+                startActivity(intent);
+                break;
+
+        }
+drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int id = item.getItemId();
 
-        boolean FragmentoTrans= false;
-        Fragment fragment=null;
-
-        if (id == R.id.menu_nueva_rutas) {
-            Intent intent = new Intent(menuActivity.this, SegrutaActivity.class);
-            startActivity(intent);
-            return true;
-        } else if (id == R.id.menu_comentar_ruta) {
-            Intent intent = new Intent(menuActivity.this, SegrutaActivity.class);
-            startActivity(intent);
-            return true;
-        }
-        switch (item.getItemId()) {
+        switch (item.getItemId()){
             case android.R.id.home:
                 drawerLayout.openDrawer(GravityCompat.START);
                 return true;
         }
-        DrawerLayout drawer= (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
         return super.onOptionsItemSelected(item);
     }
 }
